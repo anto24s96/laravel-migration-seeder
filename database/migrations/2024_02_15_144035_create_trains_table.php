ad579f19cc893e0c46ef0afb6962a19c3ff71a41
anto24s96 <anto24s96@gmail.com>
2024-02-15 15:59:05 +0100

    [UPDATE] updated trains table

--- database/migrations/2024_02_15_144035_create_trains_table.php
+++ database/migrations/2024_02_15_144035_create_trains_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 150);
+            $table->string('arrival_station', 150);
+            $table->date('departure_date');
+            $table->date('arrival_date');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 10)->unique();
+            $table->tinyInteger('carriage_number');
+            $table->boolean('in_time')->default(true);
+            $table->boolean('cancelled')->default(false);
             $table->timestamps();
         });
     }
